61173d7541e7a63d90d8466c68755dc46a110b5f
Marco Sabatini <marco.sabatini@external-rumbo.com>
2017-11-17 15:07:25 +0100

    test refactoring. Now dictionary is passed in construction.

--- js/romanNumber.js
+++ js/romanNumber.js
@@ -3,35 +3,37 @@ function reductionIsZero(reduction, dictionaryItemDecimal) {
 }
 
 
+const dictionary = [
+    {
+        decimal: 10,
+        roman: 'X'
+    },
+    {
+        decimal: 5,
+        roman: 'V'
+    },
+    {
+        decimal: 4,
+        roman: 'IV'
+    },
+    {
+        decimal: 3,
+        roman: 'III'
+    },
+    {
+        decimal: 2,
+        roman: 'II'
+    },
+    {
+        decimal: 1,
+        roman: 'I'
+    }
+];
+
 class RomanNumber {
 
-    constructor() {
-        this.dictionary = [
-            {
-                decimal: 10,
-                roman: 'X'
-            },
-            {
-                decimal: 5,
-                roman: 'V'
-            },
-            {
-                decimal: 4,
-                roman: 'IV'
-            },
-            {
-                decimal: 3,
-                roman: 'III'
-            },
-            {
-                decimal: 2,
-                roman: 'II'
-            },
-            {
-                decimal: 1,
-                roman: 'I'
-            }
-        ];
+    constructor(dictionary) {
+        this.dictionary = dictionary;
     }
 
     from(decimal) {
--- test/romanNumber.test.js
+++ test/romanNumber.test.js
@@ -2,53 +2,57 @@
 
 describe('roman number generator', function () {
 
+    let romanNumber;
+
+    beforeEach(function () {
+        romanNumber = new RomanNumber(dictionary);
+    });
+
     it('1 is I', function () {
-        expect(new RomanNumber().from(1)).equal("I");
+        expect(romanNumber.from(1)).equal("I");
     });
 
     it('5 is V', function () {
-        expect(new RomanNumber().from(5)).equal("V");
+        expect(romanNumber.from(5)).equal("V");
     });
 
     it('10 is X', function () {
-        expect(new RomanNumber().from(10)).equal("X");
+        expect(romanNumber.from(10)).equal("X");
     });
 
     it('7 is VII', function () {
-        expect(new RomanNumber().from(7)).equal("VII");
+        expect(romanNumber.from(7)).equal("VII");
     });
 
     it('8 is VIII', function () {
-        expect(new RomanNumber().from(8)).equal("VIII");
+        expect(romanNumber.from(8)).equal("VIII");
     });
 
     it('11 is XI', function () {
-        expect(new RomanNumber().from(11)).equal("XI");
+        expect(romanNumber.from(11)).equal("XI");
     });
 
     it('12 is XII', function () {
-        expect(new RomanNumber().from(12)).equal("XII");
+        expect(romanNumber.from(12)).equal("XII");
     });
 
     it('13 is XIII', function () {
-        expect(new RomanNumber().from(13)).equal("XIII");
+        expect(romanNumber.from(13)).equal("XIII");
     });
 
     it('14 is XIV', function () {
-        expect(new RomanNumber().from(14)).equal("XIV");
+        expect(romanNumber.from(14)).equal("XIV");
     });
 
     it('15 is XV', function () {
-        expect(new RomanNumber().from(15)).equal("XV");
+        expect(romanNumber.from(15)).equal("XV");
     });
 
     it('16 is XVI', function () {
-        expect(new RomanNumber().from(16)).equal("XVI");
+        expect(romanNumber.from(16)).equal("XVI");
     });
 
     it('21 is XXI', function () {
-        expect(new RomanNumber().from(21)).equal("XXI");
+        expect(romanNumber.from(21)).equal("XXI");
     });
-
-
 })
